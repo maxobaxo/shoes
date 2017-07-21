cbfcf67127ef338f2b10690c92dd7a402e20cd10
Max Scher <maxobaxo@gmail.com>
2017-07-21 11:15:43 -0700

    create page to view list of current brands

--- app/app.php
+++ app/app.php
@@ -16,54 +16,61 @@
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array("twig.path" => __DIR__."/../views"));
 
+// HOME
     $app->get('/', function() use ($app) {
         return $app['twig']->render('index.html.twig');
     });
 
+// View current list of stores
     $app->get('/stores', function() use ($app) {
         return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
     });
 
+// After adding a new store,  view updated list of stores
     $app->post('/stores', function() use ($app) {
         $new_store = new Store($_POST['name'], $_POST['city']);
         $new_store->save();
-
         return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
     });
 
+// After updating a store, view the updated list of stores
     $app->patch('/stores/{id}', function($id) use ($app) {
         $store = Store::find($id);
         $new_name = $_POST['new_name'];
         if ($new_name) {
             $store->updateName($new_name);
         }
-
         $new_city = $_POST['new_city'];
         if ($new_city) {
             $store->updateCity($new_city);
         }
-
         return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
     });
 
+// After deleting all the stores, view the empty list of stores
     $app->delete('delete_stores', function() use($app) {
         Store::deleteAll();
-
         return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
     });
 
+// After deleting a single store, view the updated list of stores
     $app->delete('/stores', function() use ($app) {
         $store_id = $_POST['store_id'];
         $store = Store::find($store_id);
         $store->delete();
-
         return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
     });
 
+// View store-specific info, with options to update/delete said store
     $app->get('/stores/{id}', function($id) use ($app) {
         $store = Store::find($id);
 
         return $app['twig']->render('store.html.twig', array('store' => $store));
+    });
+
+// View current list of brands
+    $app->get('/brands', function() use ($app) {
+        return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll()));
     });
 
     return $app;
