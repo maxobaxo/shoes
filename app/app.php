79f36aa11c57e899d887a4f9393fc5a9f8d842ee
Max Scher <maxobaxo@gmail.com>
2017-07-21 09:17:12 -0700

    integrate src/Store.php into Silex with index.html.twig

--- app/app.php
+++ app/app.php
@@ -1,3 +1,25 @@
-$app = new Silex\Application();
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../src/Store.php';
+    require_once __DIR__.'/../src/Brand.php';
 
-$app->register(new Silex\Provider\TwigServiceProvider(), array("twig.path" => __DIR__."/../views"));
+    $server = 'mysql:host=localhost:8889;dbname=shoes';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app = new Silex\Application();
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array("twig.path" => __DIR__."/../views"));
+
+    $app->get('/', function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    });
+
+
+    return $app;
+?>
